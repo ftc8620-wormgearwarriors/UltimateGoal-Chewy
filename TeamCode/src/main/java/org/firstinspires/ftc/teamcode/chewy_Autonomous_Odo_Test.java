bf654b1f99488e4ad466641d801f8f97234fa69e
jonklingensmith <jonklingensmith@gmail.com>
2021-03-10 19:13:12 -0600

    more odometry testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chewy_Autonomous_Odo_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chewy_Autonomous_Odo_Test.java
@@ -21,8 +21,6 @@ public class chewy_Autonomous_Odo_Test extends chewy_AutonomousMethods {
         initOdometryHardware(55, 9, 0);
 
 
-
-
         /** Wait for the game to begin */
         telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
@@ -36,9 +34,9 @@ public class chewy_Autonomous_Odo_Test extends chewy_AutonomousMethods {
         telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
         telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
 
-        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate());
-        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate());
-        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation());
+        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
+        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
+        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
 
 
         //Update values
@@ -48,129 +46,107 @@ public class chewy_Autonomous_Odo_Test extends chewy_AutonomousMethods {
         double dRobotPower = 0.9;
 
 
-
         //Update values
         telemetry.update();
 
-
         for(int i = 0; i<3; i++) {
-
-
-            //drive forword
-            //goToPostion(55 * robot.COUNTS_PER_INCH, 81 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-
-            //drive left  to start spot
-            goToPostion(7 * robot.COUNTS_PER_INCH, 9 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-
-            // display location
-//            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
+            //drive in diamond to test heading
+            goToPostion(24 * robot.COUNTS_PER_INCH, 134 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
+            //Display raw values
+            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
+            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
+            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
 
             telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
             telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
             telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
+
+            //Update values
             telemetry.update();
 
-            // sleep so you can see the numbers
-            sleep(5000);
+            sleep(3000);
 
 
-            //drive backword to start spot
-            goToPostion(55 * robot.COUNTS_PER_INCH, 9 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-
-            // display location
-//            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
+            //2nd pos
+            goToPostion(42 * robot.COUNTS_PER_INCH, 64 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
+            //Display raw values
+            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
+            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
+            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
 
             telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
             telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
             telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
+
+            //Update values
             telemetry.update();
 
-            // sleep so you can see the numbers
-            sleep(5000);
+            sleep(3000);
 
 
+            //3rd pos
+            goToPostion(21.5 * robot.COUNTS_PER_INCH, 37 * robot.COUNTS_PER_INCH, dRobotPower, 0, 1 * robot.COUNTS_PER_INCH, false);
+            //Display raw values
+            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
+            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
+            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
 
+            telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
+            telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
+            telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
+
+            //Update values
+            telemetry.update();
+
+            sleep(3000);
         }
 
 
+        //        for(int i = 0; i<3; i++) {
+//
+//
+//            //drive forword
+//            //goToPostion(55 * robot.COUNTS_PER_INCH, 81 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
+//
+//            //drive left  to start spot
+//            goToPostion(7 * robot.COUNTS_PER_INCH, 9 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
+//
+//            // display location
+////            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
+////            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
+////            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
+//
+//            telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
+//            telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
+//            telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
+//            telemetry.update();
+//
+//            // sleep so you can see the numbers
+//            sleep(5000);
+//
+//
+//            //drive backword to start spot
+//            goToPostion(55 * robot.COUNTS_PER_INCH, 9 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
+//
+//            // display location
+////            telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
+////            telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
+////            telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
+//
+//            telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate() / robot.COUNTS_PER_INCH);
+//            telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate() / robot.COUNTS_PER_INCH);
+//            telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation() / robot.COUNTS_PER_INCH);
+//            telemetry.update();
+//
+//            // sleep so you can see the numbers
+//            sleep(5000);
+//
+//
+//
+//
 
 
 
-//
-//        //drive in diamond to test heading
-////        goToPostion(31 * robot.COUNTS_PER_INCH, 33 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-//        //Display raw values
-//        telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//        telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//        telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
-//
-//        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate());
-//        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate());
-//        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation());
-//
-//        //Update values
-//        telemetry.update();
-//
-//        sleep(3000);
-//
-//
-//        //2nd pos
-////        goToPostion(55 * robot.COUNTS_PER_INCH, 57 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-//        //Display raw values
-//        telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//        telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//        telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
-//
-//        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate());
-//        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate());
-//        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation());
-//
-//        //Update values
-//        telemetry.update();
-//
-//        sleep(3000);
-//
-//
-//
-//        //3rd pos
-////        goToPostion(79 * robot.COUNTS_PER_INCH, 33 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-//        //Display raw values
-//        telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//        telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//        telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
-//
-//        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate());
-//        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate());
-//        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation());
-//
-//        //Update values
-//        telemetry.update();
-//
-//        sleep(3000);
-//
-//
-//        //4th pos
-////        goToPostion(55 * robot.COUNTS_PER_INCH, 9 * robot.COUNTS_PER_INCH, dRobotPower, 0, 3 * robot.COUNTS_PER_INCH, false);
-//        //Display raw values
-//        telemetry.addData("Vertical Left Position", robot.verticalLeft.getCurrentPosition());
-//        telemetry.addData("Vertical Right Position", robot.verticalRight.getCurrentPosition());
-//        telemetry.addData("Horizontal Position", robot.horizontal.getCurrentPosition());
-//
-//        telemetry.addData("X Position", robot.globalPositionUpdate.returnXCoordinate());
-//        telemetry.addData("Y Position", robot.globalPositionUpdate.returnYCoordinate());
-//        telemetry.addData("Heading", robot.globalPositionUpdate.returnOrientation());
-//
-//        //Update values
-//        telemetry.update();
-//
-//        sleep(3000);
-//
-//
-//
 //        // test pretending drop 1st wobble
 ////        goToPostion(48 * robot.COUNTS_PER_INCH, 115 * robot.COUNTS_PER_INCH, dRobotPower,  0, 3 * robot.COUNTS_PER_INCH, false);
 //
